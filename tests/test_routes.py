b65a1d8ded33deeb1cd154bd358b35aa8b4a5481
photonicVision <rabometrics@gmail.com>
2022-12-16 18:22:39 -0500

    Added badge for GitHub Actions

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -127,10 +127,10 @@ class TestAccountService(TestCase):
     def test_get_account_list(self):
         """It should get a list of accounts"""
         self._create_accounts(5)
-        resp=self.client.get(BASE_URL)
+        resp = self.client.get(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
-        data=resp.get_json()
-        self.assertEqual(len(data),5)
+        data = resp.get_json()
+        self.assertEqual(len(data), 5)
 
     def test_get_account(self):
         """It should Read a single Account"""
@@ -161,7 +161,7 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         updated_account = resp.get_json()
         self.assertEqual(updated_account["name"], "Something Known")
-        
+
     def test_delete_account(self):
         """It should Delete an Account"""
         account = self._create_accounts(1)[0]
@@ -172,4 +172,3 @@ class TestAccountService(TestCase):
         """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-        
